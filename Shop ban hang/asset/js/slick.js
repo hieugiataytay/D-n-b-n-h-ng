07107b0a80319a15f09d8196c0cb6b7fd4778d59
hieugiataytay <hieuugia20@gmail.com>
2023-07-10 01:04:37 +0700

    update

--- Shop ban hang/asset/js/slick.js
+++ Shop ban hang/asset/js/slick.js
@@ -1,10 +1,15 @@
-console.log('a')
+
 $ = jQuery
 
 $(document).ready(function(){
-    $('.home-background').slick({
+    console.log('aaas')
+     $('.home-background').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:
+        `<button type='button' class='slick-arrow slick-prev pull-left'><i class="fa-solid fa-chevron-left"></i></button>`,
+        nextArrow:
+        `<button type='button' class='slick-arrow slick-next pull-right'><i class="fa-solid fa-chevron-right"></i></button>`,
     });
 });
