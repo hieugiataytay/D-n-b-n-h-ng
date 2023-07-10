6a79ebd2f58eb25ac4d0002ad9bad19109b276eb
hieugiataytay <hieuugia20@gmail.com>
2023-07-10 20:58:05 +0700

    update

--- Shop ban hang/asset/js/slick.js
+++ Shop ban hang/asset/js/slick.js
@@ -2,10 +2,29 @@
 $ = jQuery
 
 $(document).ready(function(){
-    console.log('aaas')
-     $('.home-background').slick({
+    $('.home-background').slick({
         infinite: true,
         slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:
+        `<button type='button' class='slick-arrow slick-prev pull-left'><i class="fa-solid fa-chevron-left"></i></button>`,
+        nextArrow:
+        `<button type='button' class='slick-arrow slick-next pull-right'><i class="fa-solid fa-chevron-right"></i></button>`,
+    });
+
+    $('.slide-home-news').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:
+        `<button type='button' class='slick-arrow slick-prev pull-left'><i class="fa-solid fa-chevron-left"></i></button>`,
+        nextArrow:
+        `<button type='button' class='slick-arrow slick-next pull-right'><i class="fa-solid fa-chevron-right"></i></button>`,
+    });
+
+    $('.product-featured-slide').slick({
+        infinite: true,
+        slidesToShow: 4,
         slidesToScroll: 1,
         prevArrow:
         `<button type='button' class='slick-arrow slick-prev pull-left'><i class="fa-solid fa-chevron-left"></i></button>`,
