09c6204f41b3878a79ebba9946a248af930682d7
hieugiataytay <hieuugia20@gmail.com>
2023-07-16 01:37:33 +0700

    update

--- Shop ban hang/asset/js/slick.js
+++ Shop ban hang/asset/js/slick.js
@@ -38,21 +38,30 @@ $(document).ready(function(){
     });
 
     $('.slide-quickview-sub').slick({
-        slidesToShow: 3,
+        slidesToShow: 5,
         slidesToScroll: 1,
         asNavFor: '.slide-quickview-main',
-        centerMode: true,
         focusOnSelect: true
         });
 
     $('.slide-quickview-main').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
+        arrows: true,
         fade: true,
         asNavFor: '.slide-quickview-sub'
     });
 
+    //Chi tiết tin tức 
+    $('.post-related-slide').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:
+        `<button type='button' class='slick-arrow slick-prev pull-left'><i class="fa-solid fa-chevron-left"></i></button>`,
+        nextArrow:
+        `<button type='button' class='slick-arrow slick-next pull-right'><i class="fa-solid fa-chevron-right"></i></button>`,
+    });
  
 });
 
