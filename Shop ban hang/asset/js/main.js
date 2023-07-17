6f36c1d14a602c46442a68e83c7b964ccd17099f
hieugiataytay <hieuugia20@gmail.com>
2023-07-18 03:05:41 +0700

    update

--- Shop ban hang/asset/js/main.js
+++ Shop ban hang/asset/js/main.js
@@ -30,5 +30,24 @@ $( document ).ready(function() {
     //     }
     // )
 
+    // sticky
+    const headerMenu = $("#header-menu");
+    const content = $(".content");
+    const triggerHeight = 500; // Set the height where you want the menu to become sticky
+
+    function updateStickyHeader() {
+        if ($(window).scrollTop() >= triggerHeight) {
+        headerMenu.addClass("sticky-header");
+        content.css("padding-top", headerMenu.outerHeight() + "px");
+        } else {
+        headerMenu.removeClass("sticky-header");
+        content.css("padding-top", 0);
+        }
+    }
+
+    // Call the function on page load and when scrolling
+    $(window).on("load scroll", updateStickyHeader);
+    
+
 });
 
