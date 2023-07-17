bf6e4192de5e93548e6eb72137dc044d594a5069
hieugiataytay <hieuugia20@gmail.com>
2023-07-17 18:51:35 +0700

    k

--- Shop ban hang/asset/js/main.js
+++ Shop ban hang/asset/js/main.js
@@ -11,6 +11,19 @@ $( document ).ready(function() {
         $(this).find(".button-sub").toggleClass("change");
     });
 
+    // header menu
+    // $('.search-box-fixed').hide();
+    // $('#section-overlay').hide();
+    $(".menu-header-right .icon-glass").click(function(){
+        $('.search-box-fixed').fadeIn();
+        $('#section-overlay').addClass('open');
+    });
+
+    $(".action-close i").click(function(){
+        $('.search-box-fixed').fadeToggle();
+        $('#section-overlay').toggleClass('open');
+    });
+
     // $('#section-cart .promotion-note span').hover(
     //     function() {
     //         $('.promotion-form').toggleClass( "hover-active" );
